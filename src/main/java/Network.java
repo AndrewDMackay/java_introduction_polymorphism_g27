5cd4ff0870ef95534827f4d579ce350dd2609ea2
Andrew Mackay <andrewmackayd@gmail.com>
2021-11-15 14:12:04 +0000

    AM, 3rd commit main

--- src/main/java/Network.java
+++ src/main/java/Network.java
@@ -4,11 +4,13 @@ import java.util.*;
 public class Network {
     private String name;
     private ArrayList<IConnect> devices;
+//    private ArrayList<Desktop> devicesDesktop;
 //    private ArrayList<Printer> devicesPrinter;
 
     public Network(String name){
-        this.devicesDesktop = new ArrayList<Desktop>();
-        this.devicesPrinter = new ArrayList<Printer>();
+        this.devices = new ArrayList<>();
+//        this.devicesDesktop = new ArrayList<Desktop>();
+//        this.devicesPrinter = new ArrayList<Printer>();
         this.name = name;
     }
 
